4794f62ac8bcd138a3f0060d8aacef4ff2f8f4b0
samphathai <thao.sam@hotmail.com>
2023-02-04 21:18:57 +0700

    up load to github

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        <h2>Samphathai Wellcome</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Pos7',
+    data() {
+        return {
+
+        };
+    }
+}
+</script>
+
+<style lang="" >
+    
+</style>
